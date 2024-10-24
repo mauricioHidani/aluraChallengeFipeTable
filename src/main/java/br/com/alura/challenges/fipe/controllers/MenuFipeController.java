406c5f0fe899e14b9838bed0ebb536095044460a
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-24 09:02:45 -0300

    wip: menu fipe controller

--- src/main/java/br/com/alura/challenges/fipe/controllers/MenuFipeController.java
+++ src/main/java/br/com/alura/challenges/fipe/controllers/MenuFipeController.java
@@ -1,11 +1,32 @@
 package br.com.alura.challenges.fipe.controllers;
 
+import br.com.alura.challenges.fipe.controllers.enums.MainMenu;
+import br.com.alura.challenges.fipe.controllers.utils.ShowMenuComponent;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MenuFipeController {
 
+	private final ShowMenuComponent showMenu;
+
+	public MenuFipeController(final ShowMenuComponent showMenu) {
+		this.showMenu = showMenu;
+	}
+
 	public void start() {
+		var option = MainMenu.QUERY;
+		do {
+			option = showMenu.menuOfCode("Escolha uma operação", MainMenu.class);
+			switch (option) {
+				case QUERY -> System.out.println();
+				case QUICK_QUERY -> System.out.println();
+				case LATEST_BRAND_HISTORY -> System.out.println();
+				case LATEST_MODEL_HISTORY -> System.out.println();
+				case LATEST_STATISTICAL_HISTORY -> System.out.println();
+				case SAVE_HISTORY -> System.out.println();
+				default -> System.out.println("Opção inválida");
+			}
+		} while (option != MainMenu.EXIT);
 	}
 
 }
