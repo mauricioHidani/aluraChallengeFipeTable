63443bf7287b1451abb50e5d6261cab0848cc6bb
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-24 08:19:55 -0300

    wip: menu controller and configs

---
+++ src/main/java/br/com/alura/challenges/fipe/controllers/MenuFipeController.java
@@ -0,0 +1,11 @@
+package br.com.alura.challenges.fipe.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MenuFipeController {
+
+	public void start() {
+	}
+
+}
