af676f1c0076b297bba75773daa63e3b839f7e09
MauricioHidani <mauricio.hidani.work@gmail.com>
2024-10-24 22:49:09 -0300

    wip: add option save history

--- src/main/java/br/com/alura/challenges/fipe/controllers/MenuFipeController.java
+++ src/main/java/br/com/alura/challenges/fipe/controllers/MenuFipeController.java
@@ -12,6 +12,7 @@ public class MenuFipeController {
 	private final QueryNormalController queryNormalController;
 	private final QuickQueryController quickQueryController;
 	private final LatestHistoryController latestHistoryController;
+	private final SaveHistoryController saveHistoryController;
 
 	private History history;
 
@@ -19,12 +20,14 @@ public class MenuFipeController {
 			final ShowMenuComponent showMenu,
 			final QueryNormalController queryNormalController,
 			final QuickQueryController quickQueryController,
-			final LatestHistoryController latestHistoryController
+			final LatestHistoryController latestHistoryController,
+			final SaveHistoryController saveHistoryController
 	) {
 		this.showMenu = showMenu;
 		this.queryNormalController = queryNormalController;
 		this.quickQueryController = quickQueryController;
 		this.latestHistoryController = latestHistoryController;
+		this.saveHistoryController = saveHistoryController;
 	}
 
 	public void start() {
@@ -37,7 +40,7 @@ public class MenuFipeController {
 				case LATEST_BRAND_HISTORY -> latestHistoryController.start(history.brandHistory(), option);
 				case LATEST_MODEL_HISTORY -> latestHistoryController.start(history.modelHistory(), option);
 				case LATEST_STATISTICAL_HISTORY -> latestHistoryController.start(history.statisticalHistory(), option);
-				case SAVE_HISTORY -> System.out.println();
+				case SAVE_HISTORY -> saveHistoryController.start(history);
 				default -> System.out.println("Opção inválida");
 			}
 		} while (option != MainMenu.EXIT);
